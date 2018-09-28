fb2bc40bead02ccd119ea7caa988a41d7fadd3d1
sangjoo <dltkdltk13@naver.com>
2018-09-28 18:35:43 +0900

    Problem #10799 쇠막대기

--- Ybigorithm/CutBar.java
+++ Ybigorithm/CutBar.java
@@ -1,34 +1,19 @@
 import java.util.*;
 
-public class CutBar {
+/*
+ * Problem #10799 쇠막대기
+ * 
+ * 괄호로 입력을 받아 레이져 "()"에 잘린 쇠막대기의 잘린 조각의 개수 구하기
+ */
 
-	public static class Bar {
-		public int number;
-		public int raser;
-		public boolean open;
-		
-		Bar (int x){
-			number = x;
-			raser = 0;
-			open = true;
-		}
-		
-		void addRaser (){
-			raser++;
-		}
-		
-		void openBar () {
-			open = true;
-		}
-		
-		void closeBar (){
-			open = false;
-		}
-		
-		int getBars(){
-			return raser+1;
-		}
-	}
+
+public class CutBar {
+	
+	/*
+	 * '('가 나오면 스택에 넣고, ')'가 나오면 바로 전의 입력을 확인후
+	 * 바로전이 '('였으면 레이저라 판단하고 결과값에 +=stack.size를 취한다
+	 * 레이저가 아니라면 쇠막대기의 끝을 나타내므로 잘린 마지막 부분을 더한다 result++;
+	 */
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
@@ -36,43 +21,25 @@ public class CutBar {
 		sc.close();
 		
 		int result = 0;
-		int number = 0;
-		ArrayList <Bar> list = new ArrayList<Bar>();
-		Stack <Integer> stack = new Stack<Integer>();
 		
-		for (int i=0; i<input.length(); i++){
-			if(input.charAt(i) == '('){
-				if(input.charAt(i+1) == ')'){
-					// if it is meaning for raser
-					for(int j=0; j<list.size(); j++){
-						// increase number of raser in each of open Bar
-						if(list.get(j).open)
-							list.get(j).addRaser();
-					}
-					i++;
-				}
+		Stack <Character> stack = new Stack<>();
+		
+		for(int i=0; i<input.length(); i++) {
+			char cur = input.charAt(i);
+			if(cur == '(') {
+				if(input.charAt(i+1) != ')')
+					stack.push(cur);
+			}
+			else if(cur == ')') {
+				if(input.charAt(i-1) == '(') result += stack.size();
 				else {
-					// if it is start of Bar
-					number++;
-					list.add(new Bar(number));
-					stack.push(number);
+					stack.pop();
+					result++;
 				}
-			}
-			else if(input.charAt(i) == ')'){
-				int n = stack.pop();
-				for(int j=0; j<list.size(); j++){
-					if(list.get(j).number == n)
-						list.get(j).closeBar();
-				}
+				
 			}
 		}
-		
-		for(int j=0; j<list.size(); j++){
-			result += list.get(j).getBars();
-		}
-		
 		System.out.println(result);
-
 	}
 
 }
