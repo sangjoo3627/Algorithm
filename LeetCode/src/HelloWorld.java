ca54e6a04460d665e94957433be1ff37866b5e20
sangjoo <dltkdltk13@naver.com>
2018-09-30 12:02:18 +0900

    Test

--- LeetCode/src/HelloWorld.java
+++ LeetCode/src/HelloWorld.java
@@ -2,45 +2,12 @@ import java.util.*;
 
 
 public class HelloWorld {
-	static boolean flag;
-	public static class TreeNode {
-		int val;
-		TreeNode left;
-		TreeNode right;
-
-		TreeNode(int x) {
-			val = x;
-		}
-	}
 	
-	public static void checkTree (TreeNode a, TreeNode b){
-		if(a == null && b == null) return;
-		if(a.val == b.val){
-			if(a.left != null && b.left != null) checkTree(a.left, b.left);
-			if(a.right != null && b.right != null) checkTree(a.right, b.right);
-		}
-		else flag = false;
-	}
-	
-	public static boolean isEqualTree (TreeNode a, TreeNode b){
-		flag = true;
-		checkTree(a,b);
-		if(flag) return true;
-		else return false;
-	}
 	
 	public static void main(String[] args) {
-		TreeNode a = new TreeNode (1);
-		TreeNode b = new TreeNode (1);
-		
-		a.left = new TreeNode (1);
-		b.left = new TreeNode (1);
-		
-		Set <TreeNode> set = new HashSet();
-		set.add(a);
-		set.add(b);
-		
-		if(isEqualTree(a,b)) System.out.println("yes");
+		boolean a = false;
+		boolean b = !a;
+		System.out.println(b);
 	}
 
 }
